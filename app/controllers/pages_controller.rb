3e574dcae9e0b053494a105fc48b3c61f9f247d5
Thiago Costa <dev_thiscosta@outlook.com>
2024-07-01 21:14:21 -0700

    created controllers with methods for all models

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home, :about, :contact, :faq, :terms]
 
   def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def terms
   end
 end
